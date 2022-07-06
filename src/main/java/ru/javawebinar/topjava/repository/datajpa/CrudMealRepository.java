685eb91d4327a2b52256e3426223bb23d591b14f
Vladimir Nikitin <anakin.skk@gmail.com>
2022-07-06 16:12:11 -0500

    Fix unnecessary joins and return roles request in CrudMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,18 +13,20 @@ import java.util.List;
 
 @Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
-    Meal findByIdAndUserId(Integer id, Integer userId);
+
+    @Query("SELECT m FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+    Meal findByIdAndUserId(@Param("id") int id, @Param("userId") int userId);
 
     @Query("SELECT m FROM Meal m JOIN FETCH m.user u WHERE m.id=:id AND m.user.id=:userId")
-    @EntityGraph(attributePaths = {"user"})
-    Meal findByIdAndUserIdFetchingUserEagerly(@Param("id") Integer id, @Param("userId") Integer userId);
+    Meal findByIdAndUserIdFetchingUserEagerly(@Param("id") int id, @Param("userId") int userId);
 
     @Transactional
     @Modifying
     @Query("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
     int delete(@Param("id") int id, @Param("userId") int userId);
 
-    List<Meal> findAllByUserId(int userId, Sort sort);
+    @Query("SELECT m FROM Meal m WHERE m.user.id=:userId")
+    List<Meal> findAllByUserId(@Param("userId") int userId, Sort sort);
 
     @Query("SELECT m FROM Meal m WHERE m.user.id=:userId AND m.dateTime >= :startDateTime AND m.dateTime < :endDateTime")
     List<Meal> findAllBetweenHalfOpen(@Param("userId") int userId, @Param("startDateTime") LocalDateTime startDateTime,
