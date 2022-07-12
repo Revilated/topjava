909bb888da9d9a76b5bf7f97e26f6815464119c0
Vladimir Nikitin <anakin.skk@gmail.com>
2022-07-12 17:15:55 -0500

    Apply 6_06_HW5_optional_fetch_join.patch

--- src/main/java/ru/javawebinar/topjava/repository/UserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package ru.javawebinar.topjava.repository;
 
-import ru.javawebinar.topjava.model.*;
+import ru.javawebinar.topjava.model.User;
 
-import java.util.*;
+import java.util.List;
 
 public interface UserRepository {
     // null if not found, when updated
@@ -18,4 +18,8 @@ public interface UserRepository {
     User getByEmail(String email);
 
     List<User> getAll();
+
+    default User getWithMeals(int id) {
+        throw new UnsupportedOperationException();
+    }
 }
